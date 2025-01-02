00b477fd3edbe1d9370af41f77ec6456e5ab3bac
Maunder <maunderscott@gmail.com>
2025-01-03 02:50:29 +1300

    Adding another example

--- examples/using-table/index.ts
+++ examples/using-table/index.ts
@@ -1,3 +1,5 @@
+import { ExpressionBuilder } from "../../src/builders/expression-builder";
+import { QueryBuilder } from "../../src/builders/query-builder";
 import { Table } from "../../src/table";
 import { dbClient } from "../db-client";
 
@@ -36,6 +38,18 @@ const users = await table.nativeQuery(
 
 const fluentUsers = await table
 	.query({ pk: "users" })
+	.whereEquals("name", "Scott")
+	.useIndex("gsi1")
+	.limit(100)
+	.execute();
+
+const builder = new QueryBuilder(
+	table,
+	{ pk: "users" },
+	new ExpressionBuilder(),
+);
+
+const builtUsers = await builder
 	.whereEquals("name", "Scott")
 	.useIndex("gsi1")
 	.limit(100)
