f2d2d79a6fd3d6ec6b26e241399a067bd573668d
Maunder <maunderscott@gmail.com>
2025-01-04 00:04:24 +1300

    Setting up for consumption by other apps

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { Table } from "./table";
+export { BaseRepository } from "./repository/base-repository";
+export { ExponentialBackoffStrategy } from "./retry/exponential-backoff-strategy";
+export type { RetryStrategy } from "./retry/retry-strategy";
+export type { TableIndexConfig } from "./builders/operators";
+export {
+	DynamoError,
+	ConditionalCheckFailedError,
+	ResourceNotFoundError,
+} from "./errors/dynamo-error";
+
+export type {
+	PrimaryKey,
+	FilterOperator,
+	FilterCondition,
+} from "./builders/operators";
