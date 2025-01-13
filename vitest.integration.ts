0ccb2c5c14cce0fe49edaf285df2ce0740b088e3
Maunder <maunderscott@gmail.com>
2025-01-13 18:31:28 +1300

    Adjusting config

--- vitest.integration.ts
+++ vitest.integration.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    pool: "forks",
+    maxConcurrency: 1,
     fileParallelism: false,
     poolOptions: {
       forks: {
