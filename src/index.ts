e529ea5731d8df543b2b104b8049963bd24dd760
Scott <maunderscott@gmail.com>
2025-05-30 15:13:27 +1200

    feat(core): introduce centralized index exports and update module structure (#46)

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+// Main exports - re-export the most commonly used functionality
+export { Table } from "./table";
+export { defineEntity, createIndex, createQueries } from "./entity";
+export type {
+  EntityRepository,
+  EntityConfig,
+  QueryEntity,
+  IndexDefinition,
+  QueryRecord,
+} from "./entity";
+
+// Condition builders and types
+export {
+  eq,
+  ne,
+  lt,
+  lte,
+  gt,
+  gte,
+  between,
+  inArray,
+  beginsWith,
+  contains,
+  attributeExists,
+  attributeNotExists,
+  and,
+  or,
+  not,
+} from "./conditions";
+export type {
+  Condition,
+  ComparisonOperator,
+  LogicalOperator,
+  ConditionOperator,
+  KeyConditionOperator,
+  PrimaryKey,
+  PrimaryKeyWithoutExpression,
+  ExpressionParams,
+} from "./conditions";
+
+// Builder types
+export { QueryBuilder, type QueryOptions } from "./builders/query-builder";
+export { PutBuilder, type PutOptions } from "./builders/put-builder";
+export { UpdateBuilder, type UpdateOptions } from "./builders/update-builder";
+export { DeleteBuilder, type DeleteOptions } from "./builders/delete-builder";
+export { TransactionBuilder, type TransactionOptions } from "./builders/transaction-builder";
+
+// Utility functions for key templates
+export { partitionKey } from "./utils/partition-key-template";
+export { sortKey } from "./utils/sort-key-template";
