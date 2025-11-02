305289d4d1dd049ffa2641320adcf991ee8c0759
Maunder <smaunder@infinitystudio.ai>
2025-11-03 09:26:59 +1300

    Add the index file for builders for tsup to use

---
+++ src/builders/index.ts
@@ -0,0 +1,16 @@
+export { BatchBuilder } from "./batch-builder.js";
+export type * from "./builder-types.js";
+export { ConditionCheckBuilder } from "./condition-check-builder.js";
+export { DeleteBuilder } from "./delete-builder.js";
+export { FilterBuilder } from "./filter-builder.js";
+export { GetBuilder } from "./get-builder.js";
+export { Paginator } from "./paginator.js";
+export { PutBuilder } from "./put-builder.js";
+export { QueryBuilder } from "./query-builder.js";
+export { ResultIterator } from "./result-iterator.js";
+export { ScanBuilder } from "./scan-builder.js";
+export { TransactionBuilder } from "./transaction-builder.js";
+
+// Re-export types
+export type * from "./types.js";
+export { UpdateBuilder } from "./update-builder.js";
