e0e912459d050747857350584edca0aa4bc0ce70
Maunder <smaunder@infinitystudio.ai>
2026-05-04 18:33:05 +1200

    Adjusting the configration some more

--- src/entity.ts
+++ src/entity.ts
@@ -1,1 +1,16 @@
-export * from "./entity/entity";
+export type {
+  EntityConfig,
+  EntityDefinition,
+  EntityDeleteBuilder,
+  EntityGetBuilder,
+  EntityPutBuilder,
+  EntityRepository,
+  EntityUpdateBuilder,
+  IndexDefinition,
+  MappedQueries,
+  QueryEntity,
+  QueryFunction,
+  QueryFunctionWithSchema,
+  QueryRecord,
+} from "./entity/entity";
+export { createIndex, createQueries, defineEntity } from "./entity/entity";
